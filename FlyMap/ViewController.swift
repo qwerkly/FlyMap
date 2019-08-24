4647c467b876a51fad2ef850172e3cc6475be7fc
Ivan Babich <ivanprogun@gmail.com>
2019-08-24 19:51:37 +0300

    Initial Commit

---
+++ FlyMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlyMap
+//
+//  Created by Ivan Babich on 24/08/2019.
+//  Copyright © 2019 Ivan Babich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
