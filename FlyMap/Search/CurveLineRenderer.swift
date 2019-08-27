e0432538089ca54e897006c12aa345f5e3179a0d
Ivan Babich <ivanprogun@gmail.com>
2019-08-27 09:39:00 +0300

    init

--- FlyMap/Search/CurveLineRenderer.swift
+++ FlyMap/Search/CurveLineRenderer.swift
@@ -19,26 +19,70 @@ class CurveLineRenderer: MKPolylineRenderer {
     convenience init(overlay: MKOverlay, mapView: MKMapView) {
         self.init(overlay: overlay)
         self.mapView = mapView
+//        createPath()
     }
     
-    override func createPath() {
+    override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
+        let theMapRect = overlay.boundingMapRect
+        
+        guard mapRect.intersects(theMapRect) || self.path != nil else { return }
+        
+        //Do some logic if needed.
+        
         guard let overlay = overlay as? MKMultiPoint else { return }
         
-        var points = [MKMapPoint]()
+        var points = [CGPoint]()
         for i in 0..<overlay.pointCount {
-            points.append(overlay.points()[i])
+            points.append(point(for: overlay.points()[i]))
         }
         if points.count == 0 { return }
         
-        let path = UIBezierPath()
-        let point = mapView.convert(points[0].coordinate, toPointTo: mapView)
-        let endPoint = mapView.convert(points[1].coordinate, toPointTo: mapView)
-        print(point)
-        print(endPoint)
-        path.move(to: point)
-        path.addQuadCurve(to: endPoint, controlPoint: CGPoint(x: 200, y: 200))
+        //Create and draw your path
+        let path = CGMutablePath()
+        path.move(to: self.path.currentPoint)
         
-        self.path = path.cgPath
+//        path.addLine(to: CGPoint(x: 0.0, y: 16708800.09227179))
+        path.addLines(between: points)
+        
+        context.addPath(path)
+        
+        //Customise it
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setFillColor(UIColor.black.cgColor)
+        context.setLineWidth(2.0)
+        context.drawPath(using: .eoFillStroke)
+        
+        //And apply it
+        context.strokePath()
+    }
+    
+//    override func createPath() {
+//        super.createPath()
+//        guard let overlay = overlay as? MKMultiPoint else { return }
+//
+//        var points = [MKMapPoint]()
+//        for i in 0..<overlay.pointCount {
+//            points.append(overlay.points()[i])
+//        }
+//        if points.count == 0 { return }
+    
+//        MK
+        
+//        let path = UIBezierPath()
+//        let point = mapView.convert(points[0].coordinate, toPointTo: mapView)
+//        let endPoint = mapView.convert(points[1].coordinate, toPointTo: mapView)
+//        var center = centerBetween(coordinate1: points[0].coordinate, coordinate2: points[1].coordinate)
+//        center.latitude += 20
+//        let centerCor = mapView.convert(center, toPointTo: mapView)
+//        print(point)
+//        print(endPoint)
+//        path.move(to: point)
+//        path.addLine(to: centerCor)
+        
+//        strokeColor = .red
+//        path.addQuadCurve(to: endPoint, controlPoint: centerCor)
+        
+//        self.path = path.cgPath
 //        UIBezierPath *path = [UIBezierPath bezierPath];
 //
 //        for (NSValue *cgPointValue in cgPoints) {
@@ -50,5 +94,25 @@ class CurveLineRenderer: MKPolylineRenderer {
 //        }
 //
 //        self.path = self.tension.floatValue==0?[path bezierFlat].CGPath:[path bezierCardinalWithTension:self.tension.floatValue].CGPath;
+//    }
+    
+    private func centerBetween(coordinate1: CLLocationCoordinate2D, coordinate2: CLLocationCoordinate2D) -> CLLocationCoordinate2D {
+        let lon1 = coordinate1.longitude * .pi / 180
+        let lon2 = coordinate2.longitude * .pi / 100
+        
+        let lat1 = coordinate1.latitude * .pi / 180
+        let lat2 = coordinate2.latitude * .pi / 100
+        
+        let dLon = lon2 - lon1
+        
+        let x = cos(lat2) * cos(dLon)
+        let y = cos(lat2) * sin(dLon)
+        
+        let lat3 = atan2(sin(lat1) + sin(lat2), sqrt(cos(lat1) + x) * (cos(lat1) + x) + y * y)
+        let lon3 = lon1 + atan2(y, cos(lat1) + x)
+        
+        let center = CLLocationCoordinate2D(latitude: lat3 * 180 / .pi, longitude: lon3 * 180 / .pi)
+        
+        return center
     }
 }
--- FlyMap/Search/MapVC.swift
+++ FlyMap/Search/MapVC.swift
@@ -28,38 +28,57 @@ class MapVC: UIViewController {
         addRoute()
     }
     
+    private func deg2rad(_ number: Double) -> Double {
+        return number * .pi / 180
+    }
+    
     private func addRoute() {
-//        let path = UIBezierPath()
-//        path.move(to: CGPoint(x: 50, y: 100))
-//        path.addQuadCurve(to: CGPoint(x: 50, y: 300), controlPoint: CGPoint(x: 300, y: 200))
-//
-//        let shapeLayer = CAShapeLayer()
-//
-//        // The Bezier path that we made needs to be converted to
-//        // a CGPath before it can be used on a layer.
-//        shapeLayer.path = path.cgPath
-//
-//        // apply other properties related to the path
-//        shapeLayer.strokeColor = UIColor.blue.cgColor
-//        shapeLayer.fillColor = UIColor.clear.cgColor
-//        shapeLayer.lineWidth = 1.0
-//        shapeLayer.position = CGPoint(x: 10, y: 10)
-        
-        // add the new layer to our custom view
-//        mapView.layer.addSublayer(shapeLayer)
-        
         let startCoordinate = CLLocationCoordinate2D(latitude: startCity.latitude, longitude: startCity.longitude)
         let finishCoordinate = CLLocationCoordinate2D(latitude: finishCity.latitude, longitude: finishCity.longitude)
-        let myPolyline = MKPolyline(coordinates: [startCoordinate, finishCoordinate], count: 2)
+        let f1 = deg2rad(startCoordinate.latitude)
+        let l1 = deg2rad(startCoordinate.longitude)
+        let f2 = deg2rad(finishCoordinate.latitude)
+        let l2 = deg2rad(finishCoordinate.longitude)
+        let numberOfPoints = 20
+        let delta = (l1 - l2) / Double(numberOfPoints - 1)
+        var location = [CLLocationCoordinate2D]()
+        for i in 0..<numberOfPoints {
+            let l = l1 - delta * i
+            f = (atan())
+        }
+//        var center = centerBetween(coordinate1: startCoordinate, coordinate2: finishCoordinate)
+//        center.latitude += 1
+        
+        let myPolyline = MKPolyline(coordinates: [startCoordinate, finishCoordinate], count: 4)
+//        myPolyline.accessibilityPath
         mapView.addOverlay(myPolyline)
+    }
+    
+    private func centerBetween(coordinate1: CLLocationCoordinate2D, coordinate2: CLLocationCoordinate2D) -> CLLocationCoordinate2D {
+        let lon1 = coordinate1.longitude * .pi / 180
+        let lon2 = coordinate2.longitude * .pi / 100
+        
+        let lat1 = coordinate1.latitude * .pi / 180
+        let lat2 = coordinate2.latitude * .pi / 100
+        
+        let dLon = lon2 - lon1
+        
+        let x = cos(lat2) * cos(dLon)
+        let y = cos(lat2) * sin(dLon)
+        
+        let lat3 = atan2(sin(lat1) + sin(lat2), sqrt(cos(lat1) + x) * (cos(lat1) + x) + y * y)
+        let lon3 = lon1 + atan2(y, cos(lat1) + x)
+        
+        let center = CLLocationCoordinate2D(latitude: lat3 * 180 / .pi, longitude: lon3 * 180 / .pi)
+        
+        return center
     }
 }
 
 extension MapVC: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let renderer = CurveLineRenderer(overlay: overlay, mapView: mapView)
-        renderer.strokeColor = UIColor.green
-        renderer.createPath()
+//        renderer.strokeColor = .red
         return renderer
     }
 }
